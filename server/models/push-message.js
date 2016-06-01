3e73fe3877d2285ae14a6b87fae79b0584f3612b
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2016-06-01 14:19:49 +0200

    Correction bug test

--- server/models/push-message.js
+++ server/models/push-message.js
@@ -26,7 +26,8 @@ module.exports = function(PushMessage) {
   PushMessage.remoteMethod('getPushMessageByInterventionAndTimestamp', {
     accepts: [
       {arg: 'idIntervention', type: 'string', required: true},
-      {arg: 'timestamp', type: 'date', http: { source: 'query' }, required: true}
+      {arg: 'timestamp', type: 'date', http: { source: 'query' },
+        required: true}
     ],
     returns: {type: 'array', root: true},
     http: {verb: 'get', path: '/intervention/:idIntervention'}
